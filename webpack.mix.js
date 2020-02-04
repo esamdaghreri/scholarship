9b21f86b2627bba8474ba3ce1fb48af719e36f2c
esamdaghreri <esam@daghreri.com>
2020-02-04 22:34:35 +0300

    add sass path to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/user/main.scss', 'public/assets/css/user/main.css')
+    .sass('resources/sass/admin/main.scss', 'public/assets/css/admin/main.css');
