1f36b881f46e932d98f8f2c97cfa808287a7f761
esamdaghreri <esam@daghreri.com>
2020-02-09 17:31:13 +0300

    add footer

---
+++ resources/views/user/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="flex flex-column center-center">
+    <p class="footer-social-media">
+        <a href="#">Twitter</a>
+    </p>
+    <p class="footer-links">
+        <a href="#">Home</a>
+        <span>|</span>
+        <a href="#">Contact</a>
+        <span>|</span>
+        <a href="#">Rules and Privecy</a>
+        <span>|</span>
+        <a href="#">Developer Team</a>
+    </p>
+    <p class="footer-company-name">
+        Scholarship &copy;2020
+    </p>
+</footer>
