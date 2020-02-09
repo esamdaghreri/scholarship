878a5cd4f9a0398a4728ea317c17c84c0751a7fc
esamdaghreri <esam@daghreri.com>
2020-02-09 17:32:09 +0300

    user home page

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,6 +1,64 @@
 @extends('user.layouts.master')
 @section('content')
-    <section class="hero-section">
-        <p>@lang('public.title')</p>
-    </section>
+    <div class="homepage flex center-center flex-column">
+        <section class="hero-section flex center-center flex-column">
+            <p>@lang('public.title')</p>
+            <a href="#" class="btn btn-primary">@lang("public.register")</a>
+        </section>
+        <section class="instruction_cards flex center-center flex-column">
+            <div class="card">
+                <div class="card-container">
+                    <div class="upper-container">
+                        <div class="image-container flex center-center">
+                            <img src="{{asset('assets/images/register_new.png')}}" alt="register_step"/>
+                        </div>
+                    </div>
+                    <div class="lower-container">
+                        <div>
+                            <h2>@lang('public.first_step')</h2>
+                            <p>@lang('public.first_step_explanation')</p>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="card-container">
+                    <div class="upper-container">
+                        <div class="image-container flex center-center">
+                            <img src="{{asset('assets/images/fill.png')}}" alt="fill_step"/>
+                        </div>
+                    </div>
+                    <div class="lower-container">
+                        <div>
+                            <h2>@lang('public.second_step')</h2>
+                            <p>@lang('public.second_step_explanation')</p>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="card-container">
+                    <div class="upper-container">
+                        <div class="image-container flex center-center">
+                            <img src="{{asset('assets/images/accept.png')}}" alt="accept_step"/>
+                        </div>
+                    </div>
+                    <div class="lower-container">
+                        <div>
+                            <h2>@lang('public.third_step')</h2>
+                            <p>@lang('public.third_step_explanation')</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="suggestion flex center-center flex-column">
+            <p>@lang('public.any_note_feel_free_to_let_us_know')</p>
+            <form action="/suggestion" method="post" class="flex center-center flex-column">
+                @csrf
+                <input type="email" name="email" class="input-text" placeholder="scholarship@example.com"/>
+                <input type="text" name="name" class="input-text" placeholder="@lang('public.your_name')"/>
+                <textarea name="content" class="input-textarea" placeholder="@lang('public.your_commend')"></textarea>
+                <button type="submit" name="submit" class="btn btn-primary">@lang('public.send')</button>
+            </form>
+        </section>
+    </div>
 @endsection
--- resources/views/user/partials/header.blade.php
+++ resources/views/user/partials/header.blade.php
@@ -6,6 +6,12 @@
         <ul>
             <li><a href="#" class="btn btn-primary">@lang('public.login')</a></li>
             <li><a href="#" class="btn btn-secondary">@lang('public.sign_up')</a></li>
+            @if(App::isLocale('ar'))
+                <li><a href="{{route('localization', 'en')}}" class="locale">@lang('public.english')</a></li>
+            @endif
+            @if(App::isLocale('en'))
+                <li><a href="{{route('localization', 'ar')}}" class="locale">@lang('public.arabic')</a></li>
+            @endif
         </ul>
     </nav>
 </header>
