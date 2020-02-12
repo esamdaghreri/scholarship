ec85df3e060fa8c865641fbd149c4977439a9a7c
esamdaghreri <esam@daghreri.com>
2020-02-12 22:56:41 +0300

    add header for login system

---
+++ resources/views/user/partials/header-auth.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="logo">
+        <a href="{{route('user.home', App::getlocale())}}"><img src="{{asset('assets/images/logo.png')}}" alt="logo"></a>
+    </div>
+    <nav>
+        <ul>
+            @if(App::isLocale('ar'))
+                <li><a href="{{route(Route::currentRouteName(), 'en')}}" class="locale">@lang('public.english')</a></li>
+            @endif
+            @if(App::isLocale('en'))
+                <li><a href="{{route(Route::currentRouteName(), 'ar')}}" class="locale">@lang('public.arabic')</a></li>
+            @endif
+        </ul>
+    </nav>
+</header>
