9e01cf5a118a419a0b739f944e6f4f881b673ab3
esamdaghreri <esam@daghreri.com>
2020-03-17 19:40:23 +0300

    add default styles, make master template and dashboard  header for admin

---
+++ resources/views/admin/partials/dashboard-header.blade.php
@@ -0,0 +1,9 @@
+<header class="admin-dashboard">
+    <nav>
+        <ul class="flex flex-column">
+            <li><a href="#"><i class="fas fa-users"></i>Users</a></li>
+            <li><a href="#"><i class="fas fa-list-alt"></i>Requests</a></li>
+            <li><a href="#"><i class="fas fa-folder-open"></i>Reporst</a></li>
+        </ul>
+    </nav>
+</header>
